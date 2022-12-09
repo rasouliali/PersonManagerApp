39a4152555d0d6529ce3b4920bc227510f390244
ali rasouli <rasouli.ali@hotmail.com>
2022-12-10 00:26:15 +0330

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,2 +1,2 @@
-this project as clean architecture code for person and her/his addresses(<a href="https://github.com/jasontaylordev/CleanArchitecture/tree/net5.0">clean code .net 5</a>).
+This project as clean architecture code for person and her/his addresses(clean code .net 5 reference project is:https://github.com/jasontaylordev/CleanArchitecture/tree/net5.0).
 JWT role base authentication is also added.
